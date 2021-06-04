2e3d581eb2a14c266e3b7e63347e27e2f673e543
fahadnasir40 <fahadnasir40@gmail.com>
2021-06-04 19:35:57 +0500

    Added Review Spoiler

--- client/src/components/Review/review.js
+++ client/src/components/Review/review.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
+import { Tooltip, OverlayTrigger, Button } from 'react-bootstrap'
+import ReviewDescription from './reviewDescription'
 class Review extends Component {
-    render() {
 
+    state = {
+        reviewsToShow: 5
+    }
+
+
+    showMore = () => {
+        this.setState({
+            reviewsToShow: this.state.reviewsToShow + 5
+        })
+    }
+
+    showLess = () => {
+        this.setState({
+            reviewsToShow: 5
+        })
+    }
+
+    filterProfanity = (comment) => {
+        var Filter = require('bad-words');
+        var customFilter = new Filter({ placeHolder: '*' });
+
+        var newBadWords = ['gandu', 'bsdk', 'bullshit'];  //add as many bad words to this list
+        let removeWords = ['sexy', '']; //add bad words to remove
+
+        customFilter.addWords(...newBadWords);
+        customFilter.removeWords(...removeWords);
+
+        return customFilter.clean(comment);
+    }
+
+    render() {
         let reviews = this.props.reviews;
+
         return (
             <div className="container m-0">
                 <div className="card p-2 ">
@@ -34,58 +67,65 @@ class Review extends Component {
                     </div>
                     <div className="card-body">
                         {
-                            reviews ?
-                                reviews.map((review, key) => (
+                            reviews.length > 0 ?
+                                <span >
+                                    {
+                                        reviews.slice(0, this.state.reviewsToShow).map((review, key) => (
 
-                                    <section key={key}>
+                                            <section key={key}>
 
-                                        <div className="row font-text review ">
-                                            <div className="col-9">
-                                                <h6 className="m-0">{review.heading}</h6>
-                                                <div className="p-0 m-0 text-nowrap">
-                                                    <small>added by <span className="font-weight-bold">{review.userName}</span> on <Moment date={review.createdAt} format="MMM DD, YYYY" /></small>
-                                                </div>
-                                            </div>
-                                            <div className="col-3 text-right ">
-                                                <small className="font-text m-1 font-weight-bold">Rated {review.rating}/10</small>
-                                            </div>
-                                            <div className="col-12">
-                                                <span className='text-justify font-text font-weight-regular mt-3' style={{ fontSize: '15px', fontWeight: 400 }}>
-                                                    {review.review}
-                                                </span>
-                                            </div>
-                                            <small className="col-12 text-right m-0 ">Report review</small>
-                                            <hr className="border col-10 border-light m-0 my-2" />
-                                        </div>
-                                    </section>
-
-                                ))
-
+                                                < div className="row font-text review ">
+                                                    <div className="col-9">
+                                                        <h6 className="m-0">{this.filterProfanity(review.heading)}</h6>
+                                                        <div className="p-0 m-0 text-nowrap">
+                                                            <small>added by <span className="font-weight-bold">{review.userName}</span> on <Moment date={review.createdAt} format="MMM DD, YYYY" />
+                                                                {
+                                                                    review.isSpoiler ?
+                                                                        <span className="ml-2">
+                                                                            <OverlayTrigger
+                                                                                key={key}
+                                                                                placement={'top'}
+                                                                                overlay={
+                                                                                    <Tooltip id={`tooltip-top`}>
+                                                                                        This review is marked as spoiler by the author.
+                                                                                    </Tooltip>
+                                                                                }
+                                                                            >
+                                                                                <span className="text-danger"><i class="fa fa-info-circle " > </i>  Spoiler</span>
+                                                                            </OverlayTrigger>
+                                                                        </span>
+                                                                        : null
+                                                                }
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-3 text-right ">
+                                                        <small className="font-text m-1 font-weight-bold">Rated {review.rating}/10</small>
+                                                    </div>
+                                                    <div className="col-12 mt-2 pr-5">
+                                                        <span className='text-justify font-text font-weight-regular mt-3 ' style={{ fontSize: '16px', fontWeight: 400 }}>
+                                                            {
+                                                                <ReviewDescription review={review} filterProfanity={this.filterProfanity} />
+                                                            }
+                                                        </span>
+                                                    </div>
+                                                    <small className="col-12 text-right m-0 ">Report review</small>
+                                                    <hr className="border col-10 border-light m-0 my-2" />
+                                                </div >
+                                            </section>
+                                        ))}
+                                    {this.state.reviewsToShow < reviews.length ?
+                                        <div className="row"><div className="col fw-500 showmore text-center"><span onClick={this.showMore}>SHOW MORE</span></div></div>
+                                        : (reviews.length > 5 || (reviews.length == this.state.reviewsToShow && reviews.length > 5)) ?
+                                            <div className="row"><div className="col fw-500 showmore text-center"><span onClick={this.showLess}>SHOW LESS</span></div></div>
+                                            : null}
+                                </span>
                                 : <div>
                                     No reviews available
                             </div>
+
                         }
 
-                        {/* <div className="row font-text review my-1 ">
-                            <div className="col-9">
-                                <h6 className="m-0 text-break ">Random, but enjoyable.</h6>
-                                <div className="p-0 m-0 ">
-                                    <small>added by <span className="font-weight-bold">user2</span> on December 28,2020 </small>
-                                </div>
-
-                            </div>
-                            <div className="col-3 text-right ">
-                                <small className="font-text m-1 font-weight-bold">Rated 8/10</small>
-                            </div>
-                            <div className="col-12">
-                                <span className='text-justify font-text font-weight-regular mt-3' style={{ fontSize: '15px', fontWeight: 400 }}>
-                                    I loved this movie, but it isn't for everyone. It's got a nonsensical plot and the humour is so random that not everyone is going to like it. It seems to be made more so for people who watched the series when they were kids and are now grown up than for kids now, many of the jokes didn't have much that a kid could grasp on to compared to the series and original movie (I haven't seen the second movie as of writing this review).
-
-                                    That said I had so much fun watching it, probably the most joyous time I've spent watching something this year, and fun is really what watching a Spongebob movie is all about.
-                                 </span>
-                            </div>
-                            <small className="col-12 text-right m-0">Report review</small>
-                        </div> */}
                     </div>
                 </div>
             </div>
